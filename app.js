b38438ab21f36f82141c8217e058aa786ca0c16a
set.ortiz-monasterio <set.ortiz-monasterio@mongodb.com>
2021-05-28 15:49:53 -0300

    add typedef and resolver for query a car and the list of cars

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
 const express = require('express');
 const app = express();
 const { ApolloServer, gql } = require('apollo-server-express');
+const data = require('./data');
+const users = data.users;
+const cars = data.cars;
+const me = users[0];
 
 const typeDefs = gql`
     type Query {
+        users:[User]
+        user(id:Int!):User
         me: User
+        cars:[Car]
+        car(id:Int!):Car
     }
 
     type User{
+        id:Int!
         name: String!
+    }
+
+    type Car{
+        id:Int!
+        make:String!
+        model:String!
+        colour:String!
     }
 `;
 const resolvers = {
-    Query:{
-        me:()=>{
-            return {
-                name:'Susan'
-            }
-        }
+    Query: {
+        users: () => users,
+        user: (parent, args) => users.filter(u => u.id === args.id)[0],
+        me: () => me,
+        car: (parent, args) => cars.filter(c => c.id === args.id)[0],
+        cars:()=>cars
     }
 };
 
@@ -25,8 +41,8 @@ const server = new ApolloServer({
     typeDefs,
     resolvers
 });
-server.applyMiddleware({app});
+server.applyMiddleware({ app });
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log('listen in port 3000');
 });
---
+++ data.js
@@ -0,0 +1,13 @@
+const users = [
+    { id: 1, name: 'Set' },
+    { id: 2, name: 'Susan' },
+    { id: 3, name: 'Steven' }
+];
+
+const cars = [
+    { id: 1, make: 'Toyota', model: 'Corolla', colour: 'black' },
+    { id: 2, make: 'Chevrolet', model: 'Camaro', colour: 'white' },
+    { id: 3, make: 'BMW', model: 'BMW X1', colour: 'grey' }
+];
+
+module.exports = { users, cars };
